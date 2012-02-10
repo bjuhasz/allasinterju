fd4e23bd4448e07e542742ce1dbc69b17d8bb732
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-10 12:02:38 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Negyedik::Application.config.secret_token = 'aec92796308f16c2176b405f43904419ed521444c5c2cfd8ad0c8aefbc14f583616d5e413ffd4aec4b3e5aac878cac1f4f70c19257f015ea13584b0dc67b97ae'
