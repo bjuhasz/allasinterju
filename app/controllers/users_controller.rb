5b2bd7e9a279f926a8f065a67b82840ae0770564
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-10 13:29:56 +0100

    kesz

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @user = User.new
 
     respond_to do |format|
       format.html # index.html.erb
@@ -39,7 +40,7 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
+  def create_orig
     status = sanitize(params[:user].delete(:status))
     @user = User.new(params[:user])
     @user.status = status
@@ -51,6 +52,62 @@ class UsersController < ApplicationController
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  respond_to :html, :json
+  
+  def create
+    status = sanitize(params[:user].delete(:status))
+    @user = User.new(params[:user])
+    @user.status = status
+  
+    if @user.save
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render :partial => "users/show", :locals => { :user => @user }, :layout => false, :status => :created
+          else
+            redirect_to @user
+          end
+        end
+      end
+    else
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render :json => @user.errors, :status => :unprocessable_entity
+          else
+            render :action => :new, :status => :unprocessable_entity
+          end
+        end
+      end
+    end
+  end
+  
+  def create_comment
+    @comment = Comment.new( params[:comment] )
+  
+    if @comment.save
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render :partial => "comments/show", :locals => { :comment => @comment }, :layout => false, :status => :created
+          else
+            redirect_to @comment
+          end
+        end
+      end
+    else
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render :json => @comment.errors, :status => :unprocessable_entity
+          else
+            render :action => :new, :status => :unprocessable_entity
+          end
+        end
       end
     end
   end
